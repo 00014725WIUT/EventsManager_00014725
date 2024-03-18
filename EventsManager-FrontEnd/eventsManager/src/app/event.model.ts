d70c75249abbc6a73d8971ac1d91994d6142b8d8
00014725WIUT <wiut00014725@gmail.com>
2024-03-18 17:50:19 +0500

    initial components

---
+++ EventsManager-FrontEnd/eventsManager/src/app/event.model.ts
@@ -0,0 +1,9 @@
+// WIUT STUDENT ID: 00014725
+export interface Event {
+    EventId: number;
+    EventName: string;
+    EventDate: Date;
+    Location: string;
+    Cost: number;
+    Language: string
+}
