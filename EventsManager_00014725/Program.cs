f974a7640154505f3ccf2d28fedcfecfa108e1f9
00014725WIUT <wiut00014725@gmail.com>
2024-03-18 17:21:06 +0500

    Program.cs Changes

--- EventsManager_00014725/Program.cs
+++ EventsManager_00014725/Program.cs
@@ -3,7 +3,29 @@ using EventsManager_00014725.Models;
 using EventsManager_00014725.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+// WIUT STUDENT ID: 00014725
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+
+{
+
+    options.AddPolicy(MyAllowSpecificOrigins,
+               policy =>
+               {
+                   policy.WithOrigins("http://localhost:4200")
+
+                           .AllowAnyHeader()
+
+                           .AllowAnyMethod()
+
+                           .AllowAnyOrigin();
+
+               });
+});
 
 // Add services to the container.
 
@@ -12,13 +34,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+// WIUT STUDENT ID: 00014725
 
 builder.Services.AddDbContext<GeneralDbContext>(
     o => o.UseSqlServer(
         builder.Configuration.GetConnectionString("SqlServerConnection")));
 builder.Services.AddScoped<IRepository<Events>, EventRepository>();
 builder.Services.AddScoped<IRepository<User>, UserRepository>();
+
+
+
 
 var app = builder.Build();
 
@@ -28,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
